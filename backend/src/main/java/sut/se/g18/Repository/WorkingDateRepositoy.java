5b242a963664b3b2693d34ff67541af8ee72894e
panthiwa <thiwab59@gmail.com>
2019-01-22 19:09:43 +0700

    ทำการเพิ่ม Repository - close #57

---
+++ backend/src/main/java/sut/se/g18/Repository/WorkingDateRepositoy.java
@@ -0,0 +1,12 @@
+package sut.se.g18.Repository;
+import sut.se.g18.Entity.WorkingDateEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.web.bind.annotation.CrossOrigin;
+
+@RepositoryRestResource
+@CrossOrigin(origins = "http://localhost:4200")
+
+public interface WorkingDateRepositoy  extends JpaRepository<WorkingDateEntity, Long>{
+    WorkingDateEntity findByTypeworkingDateId(Long typeworkingDateId);
+}
