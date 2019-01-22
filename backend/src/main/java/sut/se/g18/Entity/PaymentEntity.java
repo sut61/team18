2039c71ffb8d196ae646bfc530297e66a7274cba
BallDonlawat <goodfmlove555@gmail.com>
2019-01-22 22:36:28 +0700

    เพิ่มไฟล์ PaymentEntity.java - close #64

---
+++ backend/src/main/java/sut/se/g18/Entity/PaymentEntity.java
@@ -0,0 +1,101 @@
+package sut.se.g18.Entity;
+
+import lombok.*;
+import javax.persistence.*;
+
+@Data
+@Entity
+@Table(name = "payment")
+@NoArgsConstructor
+@ToString
+@EqualsAndHashCode
+@Getter @Setter
+public class PaymentEntity {
+    @Id
+    @SequenceGenerator(name = "pay_seq",sequenceName = "pay_seq")
+    @GeneratedValue(strategy = GenerationType.SEQUENCE,generator = "pay_seq")
+    @Column(name = "pay_id",unique = true, nullable = false)
+    private @NonNull Long payId;
+    private @NonNull String name;
+    private @NonNull String address;
+    private @NonNull String phonenum;
+    private @NonNull String accountNumber;
+
+    
+
+    
+    //many-to-one  with bankEntity
+    @ManyToOne(fetch = FetchType.EAGER ,targetEntity = BankEntity.class)
+    private BankEntity bankEntity;
+
+   
+
+    //many-to-one with typepaymentEntity
+    @ManyToOne(fetch = FetchType.EAGER ,targetEntity = TypepaymentEntity.class)
+    private TypepaymentEntity typepaymentEntity;
+
+    //one-to-one with ContractEntity
+    @OneToOne(fetch = FetchType.EAGER , targetEntity = ContractEntity.class)
+    private ContractEntity contractEntity;
+
+    public ContractEntity getContractEntity() {
+        return this.contractEntity;
+    }
+
+    public void setContractEntity(ContractEntity contractEntity) {
+        this.contractEntity = contractEntity;
+    }
+    public TypepaymentEntity getTypepaymentEntity() {
+        return this.typepaymentEntity;
+    }
+
+    public void setTypepaymentEntity(TypepaymentEntity typepaymentEntity) {
+        this.typepaymentEntity = typepaymentEntity;
+    }
+
+     public BankEntity getBankEntity() {
+        return this.bankEntity;
+    }
+
+    public void setBankEntity(BankEntity bankEntity) {
+        this.bankEntity = bankEntity;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Long getPayId() {
+        return this.payId;
+    }
+
+    public void setPayId(Long payId) {
+        this.payId = payId;
+    }
+    public String getAddress() {
+        return this.address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getPhonenum() {
+        return this.phonenum;
+    }
+
+    public void setPhonenum(String phonenum) {
+        this.phonenum = phonenum;
+    }
+    public String getAccountNumber() {
+        return this.accountNumber;
+    }
+
+    public void setAccountNumber(String accountNumber) {
+        this.accountNumber = accountNumber;
+    }
+}
