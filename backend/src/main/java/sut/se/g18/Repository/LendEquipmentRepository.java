4bd65405d5683d476845b297ffecce2269c6ce88
kotjina <kotjikotji@gmail.com>
2019-01-25 00:10:21 +0700

    เพิ่ม LendEquipmentRepository.java - close #87

--- backend/src/main/java/sut/se/g18/Repository/LendEquipmentRepository.java
+++ backend/src/main/java/sut/se/g18/Repository/LendEquipmentRepository.java
@@ -1,16 +1,16 @@
 package sut.se.g18.Repository;
-
+import sut.se.g18.Entity.*;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.web.bind.annotation.CrossOrigin;
-import sut.se.g18.Entity.CustomerEntity;
+
 
 @RepositoryRestResource
 @CrossOrigin(origins = "http://localhost:4200")
-public interface CustomerRepository extends JpaRepository<CustomerEntity, Long> {
-    CustomerEntity findBycustomerName(String customerName);
-    CustomerEntity findBycustomerEmail(String customerEmail);
-    CustomerEntity findBycustomerId(Long customerid);
-}
+public interface LendEquipmentRepository extends JpaRepository<LendEquipmentEntity, Long>{
+    LendEquipmentEntity findBylendId(Long lendId);
 
+	
+
+}
