23ff65d4d8bfba74f9f5f86108f25ab34f0b956d
BallDonlawat <goodfmlove555@gmail.com>
2019-01-24 21:34:12 +0700

    Update (Bank, Contract, Typepayment)Repository.java - close #75

--- backend/src/main/java/sut/se/g18/Repository/ContractRepository.java
+++ backend/src/main/java/sut/se/g18/Repository/ContractRepository.java
@@ -1,11 +1,17 @@
 package sut.se.g18.Repository;
 
+import java.util.Collection;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import sut.se.g18.Entity.ContractEntity;
+import sut.se.g18.Entity.CustomerEntity;
+import sut.se.g18.Entity.PaymentStatusEntity;
 
 @RepositoryRestResource
 @CrossOrigin(origins = "http://localhost:4200")
 public interface ContractRepository extends JpaRepository<ContractEntity, Long> {
+    Collection<ContractEntity> findByCustomerAndStatus(CustomerEntity Customer, PaymentStatusEntity Status);
+                ContractEntity findBycontractId(Long contractId);
 }
