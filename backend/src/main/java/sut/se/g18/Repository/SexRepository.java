608d9c4a5a00d3185e254425b92f9673bff68c4a
mmiinnee <Minemine_2541@hotmail.com>
2019-01-24 22:28:40 +0700

    ทำการเพิ่มไฟล์ Repository ของระบบสมัครสมาชิกลูกค้า - close #40

--- backend/src/main/java/sut/se/g18/Repository/SexRepository.java
+++ backend/src/main/java/sut/se/g18/Repository/SexRepository.java
@@ -1,13 +1,16 @@
 package sut.se.g18.Repository;
-import sut.se.g18.Entity.TitleNameEntity;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import sut.se.g18.Entity.SexEntity;
 
 @RepositoryRestResource
 @CrossOrigin(origins = "http://localhost:4200")
+public
+interface SexRepository extends JpaRepository<SexEntity, Long> {
+    SexEntity findBycustomersex(String customersex);
 
-public interface TitleNameRepository  extends JpaRepository<TitleNameEntity, Long>{
-    TitleNameEntity findByTitlenameId(Long titlenameId);
-    TitleNameEntity findBytitlenameType(String titlenameType);
 }
+
+
