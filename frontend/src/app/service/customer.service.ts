72192e8f5b911f1a72a8a59b563a636ecf71de15
alexander193 <cheesedn@gmail.com>
2019-01-25 02:40:51 +0700

    ทำการแ้ไข Warning Use Equal Casting ใน Customer.Service - close #100

--- frontend/src/app/service/customer.service.ts
+++ frontend/src/app/service/customer.service.ts
@@ -1,7 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs/internal/Observable';
-
 @Injectable({
   providedIn: 'root'
 })
@@ -9,16 +8,16 @@ export class CustomerService {
   public API = '//localhost:8080';
   constructor(private http: HttpClient) { }
 
-  getTitlename():Observable<any>{
+  getTitlename(): Observable<any> {
     return this.http.get(this.API + '/TitleNameEntity');
   }
-  getCustomersex():Observable<any>{
+  getCustomersex(): Observable<any> {
     return this.http.get(this.API + '/sex');
   }
-  getCountryCode():Observable<any>{
+  getCountryCode(): Observable<any> {
     return this.http.get(this.API + '/countryCode');
   }
-  getCustomerAccount(email):Observable<any>{
-    return this.http.get(this.API + '/customer/'+email);
+  getCustomerAccount(email): Observable<any> {
+    return this.http.get(this.API + '/customer/' + email);
   }
 }
