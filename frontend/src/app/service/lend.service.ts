16e8a27747835f9074eac002da0002d0d5d816bc
kotjina <kotjikotji@gmail.com>
2019-01-25 12:49:56 +0700

    ทำ UI  - close #11

---
+++ frontend/src/app/service/lend.service.ts
@@ -0,0 +1,37 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class LendService {
+  public API = '//localhost:8080';
+  check : Boolean
+
+  constructor(private http: HttpClient) { 
+
+  }
+
+  getCleaningId() :  Observable<any>{
+    return this.http.get(this.API+'/CleaningEquipment');
+  }
+
+  getCustomerId() :  Observable<any>{
+    return this.http.get(this.API+'/Customer');
+  }
+
+  getElectricId() :  Observable<any>{
+    return this.http.get(this.API+'/ElectricalEquipment');
+  }
+
+  getMaidId() :  Observable<any>{
+    return this.http.get(this.API+'/Register');
+  }
+  
+  saveLendEquipment(data:any): Observable<any>{
+    return this.http.post(this.API+'/datalend',data);
+ }
+
+}
