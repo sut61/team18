f360a721aa06808fb7cc39c9e7e3b81562575e37
BallDonlawat <goodfmlove555@gmail.com>
2019-02-22 01:11:28 +0700

    ทำ การ ลบHardCode, เปลี่ยนการแจ้งเตือนแบบ Alert เป็น Snackbar, link UI - close #192

--- frontend/src/app/payment/payment.component.ts
+++ frontend/src/app/payment/payment.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, Input } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { PaymentService } from '../service/payment.service';
 import { Router, ActivatedRoute } from '@angular/router';
+import {MatSnackBar} from '@angular/material';
 @Component({
   selector: 'app-payment',
   templateUrl: './payment.component.html',
@@ -19,10 +20,13 @@ export class PaymentComponent implements OnInit {
     bank: null,
     typepayment: null,
     contract: null,
-    inputEmail: 'sut@gg.com'
+    inputEmail: ''
   };
   constructor(private router: Router, private route: ActivatedRoute,
-    private paymentService: PaymentService, private httpClient: HttpClient) { }
+    private paymentService: PaymentService, private httpClient: HttpClient, private snackbar: MatSnackBar) {
+    this.pay.inputEmail = this.route.snapshot.paramMap.get('inputEmail');
+    console.log(this.pay.inputEmail);
+  }
 
   ngOnInit() {
     this.paymentService.getBank().subscribe( data => {
@@ -33,29 +37,47 @@ export class PaymentComponent implements OnInit {
       this.typepayments = data;
       console.log(this.typepayments);
     });
-    this.paymentService.getContract().subscribe(data => {
+    this.paymentService.getContract(this.pay.inputEmail).subscribe(data => {
       this.contracts = data;
+      if (this.contracts.length === 0) {
+        alert('ไม่มีรายการค้างชำระ');
+      }
       console.log(this.contracts);
     });
   }
   save() {
     if (this.pay.name == null || this.pay.contract == null || this.pay.phonenum == null || this.pay.accountnumber == null
       || this.pay.address == null || this.pay.bank == null || this.pay.typepayment == null) {
-        alert('กรุณาเลือกและกรอกข้อมูลให้ครบถ้วน');
+      this.snackbar.open('กรุณากรอกข้อมูลให้ครบถ้วน', '', {
+        duration: 3500, verticalPosition: 'top',
+      });
   } else {
     this.httpClient.get('http://localhost:8080/payment/' + this.pay.inputEmail + '/' + this.pay.contract
     + '/' + this.pay.name + '/' + this.pay.address + '/' + this.pay.phonenum + '/'
     + this.pay.accountnumber + '/' + this.pay.typepayment + '/' + this.pay.bank).subscribe(
       data => {
         console.log('PUT Request is successful', data);
-        alert('ชำระเงินสำเร็จ');
-        location.reload();
+        this.snackbar.open('ชำระเงินสำเร็จ', '', {
+          duration: 3500, verticalPosition: 'top',
+        });
       },
       error => {
         console.log('Rrror', error);
-        alert('เกิดข้อผิดพลาด');
+        this.snackbar.open('เกิดข้อผิดพลาด', '', {
+          duration: 3500, verticalPosition: 'top',
+        });
       }
     );
+    this.resetfield();
   }
 }
+  resetfield() {
+    this.pay.accountnumber = null;
+    this.pay.bank = null;
+    this.pay.contract = null;
+    this.pay.phonenum = null;
+    this.pay.name = null;
+    this.pay.address = null;
+    this.pay.typepayment = null;
+  }
 }
